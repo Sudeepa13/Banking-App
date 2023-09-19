022d64614167b1048dd2490177c83695840fcf33
shashikrpet <113908588+shashikrpet@users.noreply.github.com>
2023-09-19 17:17:26 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
   key_name = "Awskeypair"
-  vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
+  vpc_security_group_ids= ["sg-038b8a41bfb3a0d60"] 
   connection {
     type     = "ssh"
     user     = "ubuntu"
