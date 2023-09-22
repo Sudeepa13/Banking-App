c771e2e77fff430aafa7831ba8ba480610db1936
Sudeepa13 <141397232+Sudeepa13@users.noreply.github.com>
2023-09-22 17:20:32 +0530

    Updated main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
-  key_name = "Awskeypair"
-  vpc_security_group_ids= ["sg-038b8a41bfb3a0d60"] 
+  key_name = "terrakey"
+  vpc_security_group_ids= ["sg-000701433eab05230"] 
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./Awskeypair.pem")
+    private_key = file("./terrakey.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -19,6 +19,6 @@ resource "aws_instance" "test-server" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking/my-serverfiles/finance-playbook.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-App/my-serverfiles/finance-playbook.yml "
   } 
 }
